98edc34a819699cee82b670fffe0f5480d6ae484
Dynamitos <dynamitos15@gmail.com>
2025-02-01 10:50:45 +0100

    adding more assets

--- src/scene/Renderer.cpp
+++ src/scene/Renderer.cpp
@@ -13,7 +13,7 @@ Renderer::Renderer()
       .direction = glm::normalize(glm::vec3(-0.4f, -0.3f, -0.2f)),
       .color = glm::vec3(1, 1, 1),
   });
-  scene->addModels(ModelLoader::loadModel("../res/models/cube.fbx"),
+  scene->addModels(ModelLoader::loadModel("../res/models/stanford-bunny.obj"),
                    glm::mat4(glm::vec4(1.0f, 0.0f, 0.0f, 0.0f), glm::vec4(0.0f, 1.0f, 0.0f, 0.0f), glm::vec4(0.0f, 0.0f, 1.0f, 0.0f),
                              glm::vec4(0.0f, 0.0f, 0.0f, 1.0f)));
   scene->generate();
